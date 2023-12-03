9588bb085afe1a8348e52fb9b6decacd8759f508
Adyatma <awan2403@gmail.com>
2023-12-03 23:04:40 +0700

    Refactor each algo

--- TE2/each algo/Original BnB.py
+++ TE2/each algo/Original BnB.py
@@ -9,23 +9,11 @@ def branchAndBound(values, startIndex, totalValue, unassignedValue, testAssigmen
         if testErr < bestErr[0]:
             bestErr[0] = testErr
             bestAssigment[:] = testAssigment[:]  # Use list slicing for a copy
-            print("be", bestErr)
+            # print("be", bestErr)
 
     else:
         testErr = abs(2 * testValue - totalValue)
         if testErr - unassignedValue < bestErr[0] and bestErr[0] != 0:
-            testAssigment[startIndex] = False
-            if testAssigment[0] == True and testAssigment[1] == True and testAssigment[2] == False:
-                print("here false")
-                print("false branch", testAssigment)
-                testErr = abs(2 * testValue - totalValue)
-                print("testErr", testErr)
-                print("unassignedValue", unassignedValue)
-                print(testErr - unassignedValue, bestErr[0])
-                branchAndBound(values, startIndex + 1, totalValue, unassignedValue,
-                               testAssigment, testValue, bestAssigment,
-                               bestErr)
-
             unassignedValue -= values[startIndex]
             testAssigment[startIndex] = True
             if testAssigment[0] == True and testAssigment[1] == True and testAssigment[2] == False:
@@ -34,7 +22,22 @@ def branchAndBound(values, startIndex, totalValue, unassignedValue, testAssigmen
             branchAndBound(values, startIndex + 1, totalValue, unassignedValue,
                            testAssigment, testValue + values[startIndex],
                            bestAssigment, bestErr)
-
+            testAssigment[startIndex] = False
+            #masuk pada branch yang mengandung kebenaran
+            #set 1 = 0, 1, 7, 8, 14, 20 = 50
+            #set 2 = 6, 12, 13, 19 = 50
+            #test assignment = [True, True, False ......]
+            #tetapi di sini karena hasil testErr terlalu kecil, makan branch ini tidak dicek lebih jauh
+            if testAssigment[0] == True and testAssigment[1] == True and testAssigment[2] == False:
+                print("here false")
+                print("false branch", testAssigment)
+                testErr = abs(2 * testValue - totalValue)
+                print("testErr", testErr)
+                print("unassignedValue", unassignedValue)
+                print(testErr - unassignedValue, bestErr[0])
+            branchAndBound(values, startIndex + 1, totalValue, unassignedValue,
+                           testAssigment, testValue, bestAssigment,
+                           bestErr)
 
 values = [0, 1, 6, 7, 8, 12, 13, 14, 19, 20]
 totalValue = sum(values)
@@ -48,5 +51,5 @@ branchAndBound(values, 0, totalValue, unassignedValue, testAssigment, 0, bestAss
 set1 = [values[i] for i in range(len(values)) if bestAssigment[i]]
 set2 = [values[i] for i in range(len(values)) if not bestAssigment[i]]
 
-print("Set 1:", set1)
-print("Set 2:", set2)
+print("Set 1:", set1, "total:", sum(set1))
+print("Set 2:", set2, "total:", sum(set2))
